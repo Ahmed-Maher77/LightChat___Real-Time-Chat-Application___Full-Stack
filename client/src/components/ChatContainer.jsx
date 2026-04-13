05b835ee69b63797028dc92c9121e4e81c6a36b5
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-13 06:41:40 +0200

    feat: refactor ChatContainer to include MessageSenderBox, ChatContainerHeader, and MessagesContainer components

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,25 +1,13 @@
-const ChatContainer = ({ onBack }) => {
+import MessageSenderBox from "./subcomponents/ChatContainer/MessageSenderBox";
+import ChatContainerHeader from "./subcomponents/ChatContainer/ChatContainerHeader";
+import MessagesContainer from "./subcomponents/ChatContainer/MessagesContainer";
+
+const ChatContainer = ({ selectedUser, onBack, setShowUserInfo }) => {
     return (
-        <div id="ChatContainer" className="absolute inset-0 h-full w-full flex-1 min-w-0 sm:static sm:inset-auto z-10 backdrop-blur-xs bg-gray-900/95 sm:bg-gray-900/50">
-            <button
-                type="button"
-                aria-label="Back to chats"
-                onClick={onBack}
-                className="absolute left-4 top-4 z-20 rounded-full p-2 hover:bg-gray-700/60 transition"
-            >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    className="h-6 w-6 text-stone-200 cursor-pointer"
-                    aria-hidden="true"
-                >
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 18l-6-6 6-6" />
-                </svg>
-            </button>
-            ChatContainer
+        <div id="ChatContainer" className="absolute inset-0 h-full w-full flex-1 min-w-0 sm:static sm:inset-auto z-10 backdrop-blur-sm sm:backdrop-blur-xs bg-gray-900/95 sm:bg-gray-900/50 p-5 pt-0">
+            <ChatContainerHeader selectedUser={selectedUser} onBack={onBack} setShowUserInfo={setShowUserInfo} />
+            <MessagesContainer />
+            <MessageSenderBox />
         </div>
     );
 };
